daf09bacc793fd239dcfce5e82bc5730c59a70dd
K-Schaeffer <kcb.schaeffer@gmail.com>
2023-02-09 23:54:07 -0300

    feat(frontend): add sort icon and sort toggle

--- packages/frontend/components/my/transaction-table.vue
+++ packages/frontend/components/my/transaction-table.vue
@@ -3,14 +3,30 @@
     <thead class="sticky top-0 bg-white">
       <tr>
         <th
-          v-for="(cell, index) in headCells"
-          :key="index"
-          class="px-6 py-3 border-t border-zinc-200 whitespace-nowrap font-semibold text-xs text-zinc-400 text-left"
+          v-for="cell in headCells"
+          :key="cell.id"
           :class="{
-            'hover:bg-slate-50 hover:cursor-pointer': cell.isClickable,
+            'hover:bg-slate-50 hover:cursor-pointer': cell.isSortable,
           }"
+          class="px-6 py-3 border-t border-zinc-200 whitespace-nowrap font-semibold text-xs text-zinc-400 text-left"
+          @click="() => cell.isSortable && emit('sort', cell.id)"
         >
-          {{ cell.text }}
+          <span class="flex">
+            {{ cell.text }}
+            <span v-show="cell.isSortable" class="flex">
+              <icon-arrow-up-outlined
+                :class="{ 'stroke-zinc-600': cell.isSorting === 'DESC' }"
+                width="14"
+                class="stroke-zinc-400 stroke-2 ml-2"
+              />
+              <!-- :class="{ 'ml-2': cell.isSorting === 'ASC' }" -->
+              <icon-arrow-down-outlined
+                :class="{ 'stroke-zinc-600': cell.isSorting === 'ASC' }"
+                width="14"
+                class="stroke-zinc-400 stroke-2"
+              />
+            </span>
+          </span>
         </th>
       </tr>
     </thead>
@@ -41,12 +57,14 @@
 </template>
 
 <script setup>
-const headCells = [
-  { text: "Reference" },
-  { text: "Category" },
-  { text: "Date", isClickable: true },
-  { text: "Amount", isClickable: true },
-];
+defineProps({
+  headCells: {
+    type: Array,
+    required: true,
+  },
+});
+
+const emit = defineEmits(["sort"]);
 
 const bodyRows = [
   [
--- packages/frontend/pages/index.vue
+++ packages/frontend/pages/index.vue
@@ -3,7 +3,27 @@
     <h1 class="text-zinc-900 font-bold pb-5">Transactions</h1>
     <my-transaction-search-input class="pb-3" />
     <div class="block max-h-[75vh] overflow-hidden overflow-y-scroll">
-      <my-transaction-table />
+      <my-transaction-table :head-cells="headCells" @sort="handleSort" />
     </div>
   </div>
 </template>
+
+<script setup>
+const headCells = reactive([
+  { text: "Reference", id: "reference" },
+  { text: "Category", id: "category" },
+  { text: "Date", id: "date", isSortable: true, isSorting: "" },
+  { text: "Amount", id: "amount", isSortable: true, isSorting: "" },
+]);
+
+const handleSort = (cellId) => {
+  const selectedCell = headCells.find((cell) => cell.id === cellId);
+
+  selectedCell.isSorting =
+    selectedCell.isSorting === ""
+      ? "DESC"
+      : selectedCell.isSorting === "ASC"
+      ? ""
+      : "ASC";
+};
+</script>
