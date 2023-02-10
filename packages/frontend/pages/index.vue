1592f3402eeb58fbfde9d7287526b30ffc0711b7
K-Schaeffer <kcb.schaeffer@gmail.com>
2023-02-10 00:58:15 -0300

    feat(frontend): mount initial table with real data
    mounting first 50 transactions on table with real data fetched from API

--- packages/frontend/pages/index.vue
+++ packages/frontend/pages/index.vue
@@ -3,18 +3,35 @@
     <h1 class="text-zinc-900 font-bold pb-5">Transactions</h1>
     <my-transaction-search-input class="pb-3" />
     <div class="block max-h-[75vh] overflow-hidden overflow-y-scroll">
-      <my-transaction-table :head-cells="headCells" @sort="handleSort" />
+      <my-transaction-table
+        :head-cells="headCells"
+        :body-rows="teste"
+        @sort="handleSort"
+      />
     </div>
   </div>
 </template>
 
 <script setup>
+import transactionsGql from "~/queries/transactions.gql";
+
 const headCells = reactive([
   { text: "Reference", id: "reference" },
   { text: "Category", id: "category" },
   { text: "Date", id: "date", isSortable: true, isSorting: "" },
   { text: "Amount", id: "amount", isSortable: true, isSorting: "" },
 ]);
+
+const {
+  data: {
+    value: { transactions },
+  },
+} = await useAsyncQuery(transactionsGql, {
+  limit: 50,
+  offset: 0,
+});
+
+const teste = [...transactions];
 
 const handleSort = (cellId) => {
   const selectedCell = headCells.find((cell) => cell.id === cellId);
