bb69532a7dc2cf1086558a391e43022e9689289b
K-Schaeffer <kcb.schaeffer@gmail.com>
2023-02-13 00:36:42 -0300

    refactor(frontend): create smaller components
    Creation of base components (not totally pure, but almost), breaking all components into small ones
    and refactoring pages

---
+++ packages/frontend/components/CategoryAutocompleteInput.vue
@@ -0,0 +1,41 @@
+<template>
+  <BaseLabeledInput
+    v-model="value"
+    :disabled="isDisabled"
+    type="text"
+    name="name"
+    label="Name"
+    class="max-h-8"
+    list="categories"
+    placeholder="Select or create a new category"
+  />
+  <datalist id="categories" class="w-full h-full">
+    <option
+      v-for="category in categories"
+      :key="category.id"
+      :value="category.id"
+    >
+      {{ category.name }}
+    </option>
+  </datalist>
+</template>
+<script setup>
+const props = defineProps({
+  modelValue: {
+    type: String,
+    default: "",
+  },
+  categories: {
+    type: Array,
+    default: () => [],
+  },
+  isDisabled: Boolean,
+});
+
+const emit = defineEmits(["update:modelValue"]);
+
+const value = computed({
+  get: () => props.modelValue,
+  set: (value) => emit("update:modelValue", value),
+});
+</script>
---
+++ packages/frontend/components/TransactionSearchInput.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <BaseLabeledInput
+      v-model="value"
+      :disabled="isDisabled"
+      label="Search"
+      type="text"
+      name="search"
+      placeholder="Search by bank, account, reference, category, date, amount, currency..."
+    >
+      <template #prepend-icon>
+        <IconOutlinedMagnify width="20" class="stroke-zinc-400 stroke-2 ml-2" />
+      </template>
+    </BaseLabeledInput>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  modelValue: {
+    type: String,
+    default: "",
+  },
+  isDisabled: Boolean,
+});
+
+const emit = defineEmits(["update:modelValue"]);
+
+const value = computed({
+  get: () => props.modelValue,
+  set: (value) => emit("update:modelValue", value),
+});
+</script>
