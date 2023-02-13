90968c0cdd787b72881c35955362c23509cce180
K-Schaeffer <kcb.schaeffer@gmail.com>
2023-02-13 00:47:19 -0300

    docs(frontend): add TODO to index test file

--- packages/frontend/tests/unit/index.spec.ts
+++ packages/frontend/tests/unit/index.spec.ts
@@ -1,6 +1,7 @@
 import { render } from "@testing-library/vue";
 import index from "./pages/index.vue";
 
+// TODO: Add tests
 describe("App", () => {
   it("renders index", () => {
     const { getByText } = render(index);
